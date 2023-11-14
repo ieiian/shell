206fc1ed5bf022d96de6496962023ba5c0b5af87
Images <63219448+ieiian@users.noreply.github.com>
2023-11-14 13:58:59 +0800

    Delete tse.sh

--- tse.sh
+++
@@ -1,3 +0,0 @@
-echo "已经转库"
-echo "请运行下面地址: "
-echo "mkdir -p /root/.redtse/ && curl -o /root/.redtse/redtse.sh https://github.com/redstarxxx/shell/raw/main/redtse.sh && chmod +x /root/.redtse/redtse.sh && /root/.redtse/redtse.sh"
